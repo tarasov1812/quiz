e03e2e0ae2edebe34e71b9c44c18166745c76a94
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-06-02 19:01:28 +0200

    use state for Quiz component

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -1,12 +1,17 @@
+import {useState} from "react";
 import Question from "./Question";
 
 const Quiz = () => {
+    const [currentQustionIndex, setCurrentQuestionIndex] = useState(0);
+    const testClick = () => {
+        setCurrentQuestionIndex(currentQustionIndex + 1);
+    }
     return (
         <div className="quiz">
             <div>
                 <div className="score">Question 1/8</div>
                 <Question/>
-                <div className="next-button">Next question</div>
+                <div className="next-button" onClick={testClick}>Next question {currentQustionIndex}</div>
             </div>
         </div>
     );
